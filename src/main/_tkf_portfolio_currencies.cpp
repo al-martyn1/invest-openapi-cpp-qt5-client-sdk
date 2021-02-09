bd12e2d9e321ef6f04513a4f29094d12f2261eb7
al-martyn1 <amart@mail.ru>
2021-02-09 16:45:13 +0300

    Initialized _tkf_portfolio_currencies sample

---
+++ src/main/_tkf_portfolio_currencies.cpp
@@ -0,0 +1,129 @@
+/*! \file
+    \brief Configs lookup test
+
+ */
+
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+
+#include <QCoreApplication>
+#include <QString>
+#include <QSettings>
+#include <QTest>
+#include <QDir>
+#include <QElapsedTimer>
+
+#include "invest_openapi/config_helpers.h"
+#include "invest_openapi/api_config.h"
+#include "invest_openapi/auth_config.h"
+#include "invest_openapi/currencies_config.h"
+
+#include "invest_openapi/invest_openapi.h"
+#include "invest_openapi/factory.h"
+#include "invest_openapi/openapi_completable_future.h"
+
+
+
+INVEST_OPENAPI_MAIN()
+{
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("_tkf_portfolio");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    QCoreApplication::setOrganizationName("al-martyn1");
+    QCoreApplication::setOrganizationDomain("https://github.com/al-martyn1/");
+
+    using std::cout;
+    using std::endl;
+
+    cout<<"Path to exe   : "<<QCoreApplication::applicationDirPath().toStdString()<<endl;
+
+    cout << endl;
+
+    namespace tkf=invest_openapi;
+    using tkf::config_helpers::lookupForConfigFile;
+    using tkf::config_helpers::FileReadable;
+
+    QStringList lookupConfSubfolders = QString("conf;config").split( ';', Qt::SkipEmptyParts );
+
+
+    auto logConfigFullFileName   = lookupForConfigFile( "logging.properties" , lookupConfSubfolders, FileReadable(), QCoreApplication::applicationDirPath(), true, -1 );
+    auto apiConfigFullFileName   = lookupForConfigFile( "config.properties"  , lookupConfSubfolders, FileReadable(), QCoreApplication::applicationDirPath(), true, -1 );
+    auto authConfigFullFileName  = lookupForConfigFile( "auth.properties"    , lookupConfSubfolders, FileReadable(), QCoreApplication::applicationDirPath(), true, -1 );
+
+    qDebug().nospace().noquote() << "Log  Config File: "<< logConfigFullFileName  ;
+    qDebug().nospace().noquote() << "API  Config File: "<< apiConfigFullFileName  ;
+    qDebug().nospace().noquote() << "Auth Config File: "<< authConfigFullFileName ;
+
+    auto apiConfig     = tkf::ApiConfig    ( apiConfigFullFileName  );
+    auto authConfig    = tkf::AuthConfig   ( authConfigFullFileName );
+    auto loggingConfig = tkf::LoggingConfig( logConfigFullFileName  );
+
+    QSharedPointer<tkf::IOpenApi> pOpenApi = tkf::createOpenApi( apiConfig, authConfig, loggingConfig );
+
+    tkf::ISanboxOpenApi* pSandboxOpenApi = tkf::getSandboxApi(pOpenApi);
+
+    if (pSandboxOpenApi)
+    {
+        pSandboxOpenApi->setBrokerAccountId( authConfig.getBrokerAccountId() );
+    }
+    else
+    {
+        pOpenApi->setBrokerAccountId( authConfig.getBrokerAccountId() );
+    }
+
+    auto // PortfolioResponse
+    portolioRes = pOpenApi->portfolio();
+
+    portolioRes->join();
+    tkf::checkAbort(portolioRes);
+
+    auto portfolio = portolioRes->value.getPayload();
+    auto /* QList<PortfolioPosition> */ portfolioPositions = portfolio.getPositions();
+
+    for( auto pos : portfolioPositions )
+    {
+        qDebug().nospace().noquote() << "";
+
+        marty::Decimal balance = pos.getBalance();
+        marty::Decimal blocked = pos.getBlocked();
+        std::string balanceStr = balance.toString();
+        if (blocked!=0)
+        {
+            balanceStr += std::string(" ( ") + blocked.toString() + std::string(" blocked)");
+        }
+
+        // QString getIsin()
+        // MoneyAmount getExpectedYield() const; ???
+        // MoneyAmount getAveragePositionPrice() const;
+        // MoneyAmount getAveragePositionPriceNoNkd() const;
+
+        qDebug().nospace().noquote() << pos.getTicker() << " (" << pos.getFigi() << ") - " 
+                                     << pos.getInstrumentType().asJson() << ": " << QString::fromStdString(balanceStr)
+                                     << " - " << pos.getName();
+    }
+
+
+    /*
+    qint32 getLots() const;
+    void setLots(const qint32 &lots);
+
+    MoneyAmount getAveragePositionPrice() const;
+    void setAveragePositionPrice(const MoneyAmount &average_position_price);
+
+    MoneyAmount getAveragePositionPriceNoNkd() const;
+    void setAveragePositionPriceNoNkd(const MoneyAmount &average_position_price_no_nkd);
+
+    QString getName() const;
+    void setName(const QString &name);
+    */
+
+    return 0;
+
+
+
+}
+
+
+
