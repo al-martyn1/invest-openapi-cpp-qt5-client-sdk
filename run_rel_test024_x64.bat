85b8e1d78d1704c99416fccf06159eea431269c5
al-martyn1 <amart@mail.ru>
2021-04-06 07:58:22 +0300

    Добавил запуск теста перфоманса в x64. Старый - в x86.

---
+++ run_rel_test024_x64.bat
@@ -0,0 +1,8 @@
+call run_test.bat test024 msvc2017 x64 Release
+call run_test.bat test025 msvc2017 x64 Release
+
+copy ^
+_run_logs\test024.log+^
+_run_logs\test025.log+^
+_run_logs\decimal_perf_test_results.txt
+
