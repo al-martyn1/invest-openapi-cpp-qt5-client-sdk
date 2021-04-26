fedd66f8b653fbc8b6ed80343ca909682f9971c9
al-martyn1 <amart@mail.ru>
2021-04-26 22:50:17 +0300

    Присунул заготовку test034.

---
+++ src/main/test034.cpp
@@ -0,0 +1,62 @@
+/*! \file
+    \brief Нахождение 1/N
+
+ */
+
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <set>
+#include <optional>
+
+#include <QCoreApplication>
+#include <QString>
+#include <QSettings>
+#include <QTest>
+#include <QDir>
+#include <QElapsedTimer>
+
+#include "invest_openapi/config_helpers.h"
+#include "invest_openapi/api_config.h"
+#include "invest_openapi/auth_config.h"
+#include "invest_openapi/currencies_config.h"
+#include "invest_openapi/balance_config.h"
+
+#include "invest_openapi/invest_openapi.h"
+#include "invest_openapi/factory.h"
+#include "invest_openapi/openapi_completable_future.h"
+
+#include "invest_openapi/database_config.h"
+#include "invest_openapi/database_manager.h"
+#include "invest_openapi/qt_time_helpers.h"
+
+#include "invest_openapi/db_utils.h"
+#include "invest_openapi/ioa_utils.h"
+#include "invest_openapi/ioa_ostream.h"
+#include "invest_openapi/ioa_db_dictionaries.h"
+
+#include "invest_openapi/console_break_helper.h"
+
+
+INVEST_OPENAPI_MAIN()
+{
+    QCoreApplication app(argc, argv);
+    QCoreApplication::setApplicationName("test034");
+    QCoreApplication::setApplicationVersion("1.0");
+
+    QCoreApplication::setOrganizationName("al-martyn1");
+    QCoreApplication::setOrganizationDomain("https://github.com/al-martyn1/");
+
+    using std::cout;
+    using std::endl;
+
+    
+    return 0;
+}
+
+
+
