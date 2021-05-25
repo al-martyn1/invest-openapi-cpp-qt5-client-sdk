24e5daacdd92f51cf19d31b668f0faa16e7bc000
al-martyn1 <amart@mail.ru>
2021-05-26 00:22:09 +0300

    Scripts&docs updated little bit

--- create_n_fill_db.bat
+++ create_n_fill_db.bat
@@ -1,3 +1,3 @@
-call run_test.bat _tkf_create_db && ^
-call run_test.bat _tkf_update_instruments && ^
-call run_test.bat _tkf_update_moex_listings
+@rem call run_test.bat _tkf_create_db &&
+@rem call run_test.bat _tkf_update_instruments &&
+@rem call run_test.bat _tkf_update_moex_listings
---
+++ update_instruments.bat
@@ -0,0 +1,1 @@
+call run_test.bat _tkf_update_instruments && call run_test.bat _tkf_update_moex_listings
